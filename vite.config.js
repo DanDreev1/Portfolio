9f229bf453a5d3820ca8186438f8bf4199513054
Daniil Andrieiev <75595828+DanDreev1@users.noreply.github.com>
2024-06-20 17:39:10 +0100

    maybe it helps :)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio/",
-  plugins: [react()],
+  plugins: [react(),],
   assetsInclude: ['**/*.glb']
-})
+});
